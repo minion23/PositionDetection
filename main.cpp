8708fa9abd6fd4a13284f8d1ef744df23b4a69c8
byyangyang <byyangyang@ucla.edu>
2015-11-03 22:32:48 -0800

    motion detection function

--- main.cpp
+++ main.cpp
@@ -65,21 +65,28 @@ inline int detectMotopn(const Mat & motion, Mat & result, Mat & result_cropped,
 int main()
 {
     VideoCapture cap(0); // open the video camera no. 0
-
     if (!cap.isOpened())  // if not success, exit program
     {
         cout << "Cannot open the video cam" << endl;
         return -1;
     }
-
-   double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-   double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-
-    cout << "Frame size : " << dWidth << " x " << dHeight << endl;
-
+    //double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+    //double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+    //cout << "Frame size : " << dWidth << " x " << dHeight << endl;
     namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    
+    Mat result, result_cropped;
+    Mat p_frame, c_frame, n_frame;
+    cap.read(p_frame);
+    result = p_frame;
+    cap.read(c_frame);
+    cap.read(n_frame);
 
-    while (1)
+    cvtColor(c_frame, c_frame, CV_RGB2GRAY);
+    cvtColor(p_frame, p_frame, CV_RGB2GRAY);
+    cvtColor(n_frame, n_frame, CV_RGB2GRAY);
+    
+    while (true)
     {
         Mat frame;
         bool bSuccess = cap.read(frame); // read a new frame from video
@@ -88,14 +95,27 @@ int main()
              cout << "Cannot read a frame from video stream" << endl;
              break;
         }
-
-        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+        //imshow("MyVideo", frame); //show the frame in "MyVideo" window
+         
+       
+       
+       
+       
+       
+       
+       
+       
+       
         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
+        {
             cout << "esc key is pressed by user" << endl;
             break; 
-       }
+        }
+
     }
+
+
+
     return 0;
 
 }
